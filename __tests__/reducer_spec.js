3e0ddae8770c381b08047fcaf0d01acf983cd345
Jay Chetty <jaypaulchetty@gmail.com>
2016-12-14 14:40:48 +0000

    start vanilla version

--- __tests__/reducer_spec.js
+++ __tests__/reducer_spec.js
@@ -1,25 +1,25 @@
 import gameReducer from '../reducers/index';
 
 test('starts with 10 cards in the deck', () => {
-  expect( gameReducer(undefined, {}).toJS().deck.length ).toBe(11);
+  expect( gameReducer(undefined, {}).deck.length ).toBe(11);
 });
 
 test('starts with 1 hidden card', () => {
-  expect( gameReducer(undefined, {}).toJS().hidden.length ).toBe(1);
+  expect( gameReducer(undefined, {}).hidden.length ).toBe(1);
 });
 
 test('starts with 3 visible cards', () => {
-  expect( gameReducer(undefined, {}).toJS().visible.length ).toBe(3);
+  expect( gameReducer(undefined, {}).visible.length ).toBe(3);
 });
 
 test('starts with 2 players', () => {
-  expect( gameReducer(undefined, {}).toJS().players.length ).toBe(2);
+  expect( gameReducer(undefined, {}).players.length ).toBe(2);
 });
 
 test('player starts with a card in their hand', () => {
-  expect( gameReducer(undefined, {}).toJS().players[0].hand.length ).toBe(1);
+  expect( gameReducer(undefined, {}).players[0].hand.length ).toBe(1);
 });
 
 test('player starts empty with discard', () => {
-  expect( gameReducer(undefined, {}).toJS().players[0].discard.length ).toBe(0);
+  expect( gameReducer(undefined, {}).players[0].discard.length ).toBe(0);
 });
--- reducers/index.js
+++ reducers/index.js
@@ -1,11 +1,9 @@
-import Immutable from 'immutable'
-
-const initalState = Immutable.fromJS({
+const initalState = {
     deck: [1,1,2,3,4,4,5,5,6,7,8],
   hidden: [1],
   visible:[1,2,3],
   players:[ { hand:[1], discard:[] },{hand:[1], discard:[]}]
-})
+}
 
 function gameReducer(state=initalState, action){
   return state;
